e849a3e20a1cfc8a436f56c9d15d3e7581a7f7f1
thehnm <thehnm@posteo.net>
2019-12-21 09:21:11 +0100

    Creates clauses now

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <vector>
 
 int64_t convertCStringToInt(const char *literal)
 {
@@ -24,13 +25,14 @@ int64_t convertCStringToInt(const char *literal)
     return x;
 }
 
-void readCNFFile(std::string filename)
+void readCNFFile(std::vector<std::vector<int64_t>> &clause, std::string filename)
 {
     std::string line;
     std::ifstream myfile;
     myfile.open(filename);
-
+    std::vector<int64_t> bufferClause;
     char *token;
+
     while (std::getline(myfile, line))
     {
         if (line[0] == 'c')
@@ -46,29 +48,40 @@ void readCNFFile(std::string filename)
         if (cLine[0] == 'p')
         {
             std::cout << "Header read." << std::endl;
+            continue;
         }
-        // Read clauses
-        else
-        {
-            token = std::strtok(cLine, " ");
-            while (token != NULL)
-            {
-                int64_t lit = convertCStringToInt(token);
-                if (lit == 0)
-                {
-                    break;
-                }
 
-                std::cout << lit << ", ";
-                token = strtok(NULL, " ");
+        // Read clauses
+        token = std::strtok(cLine, " ");
+        while (token != NULL)
+        {
+            int64_t lit = convertCStringToInt(token);
+            if (lit == 0)
+            {
+                break;
             }
-            std::cout << std::endl;
+
+            bufferClause.push_back(lit);
+            token = strtok(NULL, " ");
         }
+        clause.push_back(bufferClause);
+        bufferClause.clear();
     }
 }
 
 int main(int argc, char **argv)
 {
-    readCNFFile(argv[1]);
+    std::vector<std::vector<int64_t>> clauses;
+    std::vector<int64_t> variables;
+
+    readCNFFile(clauses, argv[1]);
+    for (std::vector<int64_t> c : clauses)
+    {
+        for (int64_t l : c)
+        {
+            std::cout << l << " ";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
